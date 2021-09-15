907c41c822650003eaab40249da55bae9c39610e
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-09-14 21:27:20 -0400

    Add files via upload
    keep just brushing up with MYSQL, all the basics, so far so good

--- sqlCourseUdemy.sql
+++ sqlCourseUdemy.sql
@@ -1,3 +1,29 @@
 /*This is a new course about learning more of SQL today i have done introducting videos
 hopefully by the end of the course i can understand better and use it in my app
+
+The instructor might have used command mysql-ctl cli to open the MySQL prompt. Then you can use those queries for MySQL.
+*/
+
+/*show database is used to show the databases created 
+ SHOW DATABASE database_name
+
+
+ show tables
+ SHOW TABLES
+
+ show columns in a table
+
+SHOW COLUMNS FROM table_name
+
+or you could use describe
+
+DESC table_name
+
+delete a table
+DROP table_name;
+
+show warning is a command tha is used when a value is enetered wrong into the table we can detect where the error 
+is coming from
+
+so far i. have been brushing up MYSQL understading more everytime
 */
