869b19d72d4e65ed25f44e71dccf019a6128d79a
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-12-01 21:58:31 -0500

    Add files via upload
    practicing queries and exercises with real problem examples

--- learning schemas.sql
+++ learning schemas.sql
@@ -63,14 +63,55 @@ USE socialMedia;
  
  
  
-/*Create a query that shows the oldes user in the database*/
+*Create a query that shows the oldes user in the database*/
  SELECT * FROM users ORDER BY created_at ASC LIMIT 5;
 
 /*Create a query that shows the most popular registration day in the database*/
 SELECT DAYNAME(created_at) AS most_used , COUNT(*) AS total FROM users GROUP BY most_used ORDER BY total DESC;
  
  /*Find the user who have never posted a photo*/
-  SELECT users.username , photos.image_url FROM users JOIN photos ON users.id != photos.user_id;
+  
+SELECT users.username , photos.image_url 
+FROM users LEFT JOIN 
+photos ON users.id = photos.user_id WHERE photos.image_url IS NULL;
+
+/*Identify most popular photo and user who created it*/ 
+SELECT
+ users.username,
+photos.id,
+COUNT(*) AS TOTAL,
+photos.image_url 
+FROM likes
+JOIN photos ON likes.photo_id = photos.id
+JOIN users ON photos.user_id = users.id 
+GROUP BY photo_id ORDER BY TOTAL DESC LIMIT 5;
+
+/*How many times does the average user post*/
+
+
+SELECT COUNT(*) FROM users;
+SELECT COUNT(*) FROM photos;
+
+SELECT 
+(SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users);
+
+/*What are the top 5 most commoly used hashtags*/
+SELECT tags.tag_name ,
+ COUNT(*) as TOTAL 
+ FROM photo_tags JOIN tags
+ ON photo_tags.tag_id = tags.id 
+ GROUP BY photo_tags.tag_id 
+ ORDER BY TOTAL DESC 
+ LIMIT 5;
  
-  select * from photos where image_url is null;
- 
+/*Find user who have liked every single photo on the site*/
+SELECT users.username , COUNT(*) AS total FROM users 
+JOIN likes ON users.id = likes.user_id
+GROUP BY likes.user_id 
+HAVING total = (SELECT COUNT(*) FROM photos)
+ORDER BY total DESC; 
+
+SELECT COUNT(*) FROM photos;
+
+
+
